57e5c5b7c1916f0abe3e4c75ddc39bebd76d3a56
Arale <arale@AraletekiMacBook-Pro.local>
2013-03-20 23:13:42 +0800

    build test dir

--- src/cluster/test.c
+++ src/cluster/test.c
@@ -5,35 +5,38 @@ void thread_fun(void* i){
     char ser[30];
     int *t = (int *)i;
     int count = 10000;
-    while(count--){
+    while(1){
         get_server_in_zk(ser,30);
+        sleep(300);
         printf("thread%d :%s\n", *t,ser);
     }
 }
 int main(int ac, char **av){
 
-	zhandle_t *zh = register_to_zk("127.0.0.1:2181", "/asncluster", argv[1]);
-    zhandle_t *zh = monitor_by_zk("127.0.0.1:2181", "/asncluster");
+    if(ac<2) {
+        printf("bad arguments\n");
+        return -1;
+    }
+	zhandle_t *zh = register_to_zk("127.0.0.1:2181", "/asncluster", av[1]);
+    zhandle_t *zh2= monitor_by_zk("127.0.0.1:2181", "/asncluster");
     int count = 10000;
     char server[30] ;
     pthread_t th1 ;
     int thread_no1 = 1;
     pthread_create(&th1, NULL,(void *)thread_fun, (void *)&thread_no1);
 
-    pthread_t th2 ;
-    int thread_no2 = 2;
-    pthread_create(&th2, NULL,(void *)thread_fun, (void *)&thread_no2);
 
-    while(count--){
+/*    while(count--){
       get_server_in_zk(server, 30);
       printf("main:%s\n", server);
-    }
+    }*/
     pthread_join(th1,NULL);
-    pthread_join(th2,NULL);
+while(1);
     zookeeper_close(zh);
+
 }
 
-*/
+
 
 /*
 int main(int ac, char **av){
