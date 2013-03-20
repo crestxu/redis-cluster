e7ef71d6b9ff0f548559f8302d406a8eace6413c
crestxu <crestxu@gmail.com>
2013-03-20 21:18:37 +0800

    add test zookeeper functions

---
+++ src/cluster/test.c
@@ -0,0 +1,45 @@
+#include "zookeeper_cluster.h"
+#include<pthread.h>
+
+void thread_fun(void* i){
+    char ser[30];
+    int *t = (int *)i;
+    int count = 10000;
+    while(count--){
+        get_server_in_zk(ser,30);
+        printf("thread%d :%s\n", *t,ser);
+    }
+}
+int main(int ac, char **av){
+
+	zhandle_t *zh = register_to_zk("127.0.0.1:2181", "/asncluster", argv[1]);
+    zhandle_t *zh = monitor_by_zk("127.0.0.1:2181", "/asncluster");
+    int count = 10000;
+    char server[30] ;
+    pthread_t th1 ;
+    int thread_no1 = 1;
+    pthread_create(&th1, NULL,(void *)thread_fun, (void *)&thread_no1);
+
+    pthread_t th2 ;
+    int thread_no2 = 2;
+    pthread_create(&th2, NULL,(void *)thread_fun, (void *)&thread_no2);
+
+    while(count--){
+      get_server_in_zk(server, 30);
+      printf("main:%s\n", server);
+    }
+    pthread_join(th1,NULL);
+    pthread_join(th2,NULL);
+    zookeeper_close(zh);
+}
+
+*/
+
+/*
+int main(int ac, char **av){
+    zhandle_t *zh = register_to_zk("127.0.0.1:2181,127.0.0.1:2182", "/asncluster", "192.168.3.1:123");
+    getchar();
+    return 0 ;
+}
+*/
+
--- src/cluster/zookeeper_cluster.h
+++ src/cluster/zookeeper_cluster.h
@@ -13,31 +13,9 @@ extern "C" zhandle_t* register_to_zk(const char* zk_host_ports, const char* path
 extern "C" zhandle_t* monitor_by_zk(const char *host_ports, const char *cluster_node);
 extern "C" int get_server_in_zk(char *host_port, int n);
 #else
-/**    提供服务方接口
-   此函数将当前服务器挂载到zk集群维护的数据结构上，这样B就能看到当前机器及提供服务的端口。
-   此函数为异步函数，只需在任何地方调用即可，不影响其他正常业务流程。它会自动保持与zk集群的心跳。
-   参数说明：
-zk_host_ports:zk集群的列表，格式如”127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183”
-path:挂载的路径，比如”/cluster”,所有提供相同服务的机器都挂载到该路径下，构成集群。
-local_host_port:本机器提供服务的ip和端口
-*/
+
 zhandle_t* register_to_zk(const char* zk_host_ports, const char* path, const char* local_host_port ) ;
-
-/**********************使用服务方接口****************/
-/*
-   在获得集群的机器前,需要首先调用此函数，连接zk集群，对提供服务方A进行监控。
-Host_ports: 集群的列表，格式如”127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183
-cluster_node: 提供服务方的挂载路径
-*/
-
 zhandle_t* monitor_by_zk(const char *host_ports, const char *cluster_node);
-
-/*
-   获得集群中的某一台可用的机器，成功返回0，失败返回-1
-Host_port: 用于返回可用的机器，该内存需要调用者分配，内部已经实现了负载均衡。返回地址格式为”127.0.0.1:2181”
-N: host_port内存大小
-*/
-
 int get_server_in_zk(char *host_port, int n);
 
 #endif
